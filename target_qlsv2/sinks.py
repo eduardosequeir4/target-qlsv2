5ccb2cce6fc61609b93c714540dcac9105ff7ae0
eduardosequeir4 <125479526+eduardosequeir4@users.noreply.github.com>
2024-02-12 15:48:41 +0000

    weekend delivery date check

--- target_qlsv2/sinks.py
+++ target_qlsv2/sinks.py
@@ -1,7 +1,7 @@
 """QlsV2 target sink class, which handles writing streams."""
 
 import ast
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from target_qlsv2.client import QlsV2Sink
 
@@ -14,6 +14,12 @@ class BuyOrdersV2Sink(QlsV2Sink):
 
     def preprocess_record(self, record: dict, context: dict) -> dict:
         dateoriginal = record["created_at"]
+        # Adjusting for Saturday and Sunday
+        if dateoriginal.weekday() == 5:  # Saturday
+            dateoriginal += timedelta(days=2)
+        elif dateoriginal.weekday() == 6:  # Sunday
+            dateoriginal += timedelta(days=1)
+    
         dateformatted = dateoriginal.strftime("%Y-%m-%d")
         deliveries = [{"estimated_arrival": dateformatted}]
         if "line_items" in record:
@@ -30,18 +36,18 @@ class BuyOrdersV2Sink(QlsV2Sink):
                     list(record["line_items"]),
                 )
             )
-
+    
             record["id"] = str(record["id"])
             payload = {"suppliers": [record["supplier_remoteId"]], "customer_title": record["id"],"pre_order": 0,"purchase_order_products": purchase_order_products, "deliveries": deliveries}
-
+    
             processed_record = {
                 "buy_order_remoteId": record["remoteId"],
                 "payload": payload,
             }
-
+    
         else:
             processed_record = None
-
+    
         return processed_record
 
     def process_record(self, record: dict, context: dict) -> None:
